59529253d79cfea16e20cf894dd12bd913f4ae71
Rafael Perez and Suzi Rubino <suzi@>
2016-11-01 18:09:28 -0700

    javascript updates to remedyammount display

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,56 @@
 
+// business logic
 
-function BankAccount(name, amount) {
+function BankAccount (name, amount) {
   this.name = name;
   this.amount = amount;
-
 }
 
 
-BankAccount.prototype.deposit = function (deposit){
-  return this.amount = deposit + this.amount;
-  console.log("function: ", this.amount);
+BankAccount.prototype.deposit = function (deposit) {
+  this.amount = this.amount + deposit;
 }
 
-BankAccount.prototype.withdrawal = function (withdrawal){
-  return this.amount = this.amount - withdrawal ;
+BankAccount.prototype.withdrawal = function (withdrawal) {
+  this.amount = this.amount - withdrawal;
 }
 
+function resetFields () {
+  $("input#userDeposit").val("");
+  $("input#userWithdrawal").val("");
+  $("input#runningBalanceWell").val("");
+}
 
+// user interface
 
+$(document).ready(function() {
 
-
-
-
-$(document).ready(function (){
-var account = new BankAccount("","");
-  $("#form1").click(function (event){
+  $("form#initialBalance").submit(function(event) {
     event.preventDefault();
 
     var userName = $("#userName").val();
-    var userInitialBalance = parseInt($("#userInitialBalance").val());
-
-    account.name = userName;
-    account.amount = userInitialBalance;
-
-    console.log(account.amount);
-//set price here, using values from our array
+    var userInitialAmount = parseInt($("#userInitialAmount").val());
+    var userBalance = new BankAccount(userName, userInitialAmount);
 
 
-    $("#runningBalanceWell").text(account.amount);
-    //call reset function
+    $("#runningBalanceWell").text(userBalance.amount);
+
     $("#openingBalance").hide();
-    $("#userNameHead").text(account.name);
+    $("#userNameHead").text(userBalance.name);
 
   });
 
-  $("#form2").click(function (event){
+  $("form#runningBalance").submit(function(event) {
     event.preventDefault();
 
     var userDeposit = parseInt($("#userDeposit").val());
     var userWithdrawal = parseInt($("#userWithdrawal").val());
+    var accountBalance = new BankAccount("","");
+    accountBalance.deposit(userDeposit);
+    accountBalance.withdrawal(userWithdrawal);
 
-    // var newBalance = new BankAccount("", );
-    console.log(account.deposit(userDeposit));
-    console.log(account.withdrawal(userWithdrawal));
-
-    $("#runningBalanceWell").text(account.amount);
+    $("#runningBalanceWell").text(accountBalance);
+    resetFields();
   });
 
 });
